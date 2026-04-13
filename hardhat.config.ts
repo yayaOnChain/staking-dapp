b2bca4dd12b115501dea071f6b3f470aedec4a52
yayaOnChain <yaya.nurcahya@gmail.com>
2026-04-13 22:32:58 +0700

    Create hardhat config

---
+++ hardhat.config.ts
@@ -0,0 +1,48 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@nomicfoundation/hardhat-verify";
+import "@typechain/hardhat";
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.20",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    // Configuration for Sepolia Testnet (Best for testing)
+    sepolia: {
+      url: process.env.ALCHEMY_API_URL || process.env.SEPOLIA_RPC_URL || "",
+      accounts:
+        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+      chainId: 11155111,
+      timeout: 60000, // 60 seconds timeout
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
+  paths: {
+    sources: "./src/contracts",
+    tests: "./src/test",
+    cache: "./cache/hardhat",
+    artifacts: "./artifacts",
+  },
+  typechain: {
+    outDir: "typechain-types",
+    target: "ethers-v6",
+  },
+};
+
+export default config;
